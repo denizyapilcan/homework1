ead3e6fb8be755fb628f847657cc90b385c52eab
denizyapilcan <39298823+denizyapilcan@users.noreply.github.com>
2018-06-05 09:54:21 +0200

    Add files via upload

---
+++ session1.py
@@ -0,0 +1,17 @@
+# def myFunction (x,y):
+#     print("Hello "+x+" and "+y)
+# myFunction ("Deniz","Cetin")
+# myString = ("Alpha Beta Gamma")
+# myString.split(" ")
+
+
+myString= "Frogs Docks Birds".split(" ")
+myString
+if "Frogs" in myString:
+    print("wrak"*3)
+else:
+def myFunction(x,y):
+    if (x) in myString:
+        print("wrak")
+else:
+        print("nope")
